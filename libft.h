4a3e704d6df3fa897c9d7eceb11a6267e0f0e074
ostruzina <cerna.ostruzina@gmail.com>
2026-09-18 19:29:17 +0200

    Improve formatting

--- libft.h
+++ libft.h
@@ -12,8 +12,8 @@ int		ft_isprint(int c);
 int		ft_isascii(int c);
 void 	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 
