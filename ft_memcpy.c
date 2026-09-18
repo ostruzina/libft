f675a1042dd9b03118bb29dedaa342f4f666cc2f
ostruzina <cerna.ostruzina@gmail.com>
2026-09-18 16:47:10 +0200

    Remove NULL check from ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: verosvec <verosvec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/16 18:40:29 by verosvec          #+#    #+#             */
-/*   Updated: 2026/09/17 19:32:38 by verosvec         ###   ########.fr       */
+/*   Updated: 2026/09/18 16:45:49 by verosvec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const unsigned char	*usrc;
 	size_t				i;
 
-	if (dest == NULL && src == NULL)
-	{
-		return (dest);
-	}
 	udest = (unsigned char *)dest;
 	usrc = (const unsigned char *)src;
 	i = 0;
