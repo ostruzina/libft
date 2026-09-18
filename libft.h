fee192ff68271c3095e883597c5f2ab94ca250f7
ostruzina <cerna.ostruzina@gmail.com>
2026-09-18 15:26:45 +0200

    Add libft.h

---
+++ libft.h
@@ -0,0 +1,18 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+void 	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+#endif
