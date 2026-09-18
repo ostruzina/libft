37ad55931c8ccf3eb89d9ccfe56619c167de0da1
ostruzina <cerna.ostruzina@gmail.com>
2026-09-18 20:15:45 +0200

    Add ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: verosvec <verosvec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/18 19:30:05 by verosvec          #+#    #+#             */
+/*   Updated: 2026/09/18 20:11:52 by verosvec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*us1;
+	const unsigned char	*us2;
+	size_t				i;
+
+	us1 = (const unsigned char *)s1;
+	us2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (us1[i] != us2[i])
+			return (us1[i] - us2[i]);
+		i++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -16,5 +16,6 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
