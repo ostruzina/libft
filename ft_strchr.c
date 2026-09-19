bf93420be5cb032b545f91f31d4334cf04bc50c4
ostruzina <cerna.ostruzina@gmail.com>
2026-09-19 18:26:41 +0200

    Improve loop logic in ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: verosvec <verosvec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/18 20:24:39 by verosvec          #+#    #+#             */
-/*   Updated: 2026/09/18 21:43:41 by verosvec         ###   ########.fr       */
+/*   Updated: 2026/09/19 18:25:52 by verosvec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,11 @@ char	*ft_strchr(const char *s, int c)
 
 	ch = (char)c;
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i] == ch || s[i] != '\0')
 	{
 		if (s[i] == ch)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (s[i] == ch)
-		return ((char *)&s[i]);
 	return (NULL);
 }
