af3a8f234871bb83304d7c0133d770a45c88b931
ostruzina <cerna.ostruzina@gmail.com>
2026-09-22 22:16:55 +0200

    Add ft_itoa

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ SRCS = ft_strlen.c ft_memset.c \
 		ft_putnbr_fd.c ft_putendl_fd.c \
 		ft_putstr_fd.c ft_strmapi.c \
 		ft_striteri.c ft_strtrim.c \
-		ft_split.c
+		ft_split.c ft_itoa.c
 OBJS = $(patsubst %.c,%.o,$(SRCS))
 
 all: $(NAME)
@@ -76,7 +76,8 @@ TEST_SRCS = tests/main.c \
 			tests/test_ft_put_fd.c \
 			tests/test_ft_strm_iter.c \
 			tests/test_ft_strtrim.c \
-			tests/test_ft_split.c
+			tests/test_ft_split.c \
+			tests/test_ft_itoa.c
 TEST_OBJS = $(patsubst %.c,%.o,$(TEST_SRCS))
 
 %.asan.o: %.c libft.h
---
+++ ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: verosvec <verosvec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/22 21:33:21 by verosvec          #+#    #+#             */
+/*   Updated: 2026/09/22 21:33:32 by verosvec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	get_num_len(long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+	{
+		len++;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	size_t	len;
+	long	num;
+
+	num = n;
+	len = get_num_len(num);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (num == 0)
+		str[0] = '0';
+	if (num < 0)
+	{
+		str[0] = '-';
+		num = -num;
+	}
+	while (num > 0)
+	{
+		str[--len] = (num % 10) + '0';
+		num /= 10;
+	}
+	return (str);
+}
